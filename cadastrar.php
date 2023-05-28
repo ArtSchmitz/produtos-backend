9f874e3afd5c2bc2b7174080da0289071ef8a694
Arthur Felipe Schmitz <109077446+ArtSchmitz@users.noreply.github.com>
2023-05-28 19:28:55 -0300

    inserindo produtos para o banco de dados

---
+++ cadastrar.php
@@ -0,0 +1,49 @@
+<?php
+
+//cabecalho obrigatorio
+header("Acess-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Acess-Control-Allow-Headers: *");
+// header("Access-Control-Allow-Methods: GET,PUT,POST,DELETE");
+
+//conexao com o db
+include_once 'conexao.php';
+
+$response_json = file_get_contents('php://input');
+$dados = json_decode($response_json, true);
+
+if ($dados){
+
+    $query_produtos = "INSERT INTO produtos (titulo, descricao) VALUES (:titulo, :descricao)";
+    $cad_produto = $conn->prepare($query_produtos);
+
+    $cad_produto->bindParam(':titulo', $dados['produto']['titulo'], PDO::PARAM_STR);
+    $cad_produto->bindParam(':descricao', $dados['produto']['descricao'], PDO::PARAM_STR);
+
+    $cad_produto->execute();
+
+    if($cad_produto->rowCount()){
+        $response = [
+            "error" => false,
+            "mensagem" => "Produto cadastrado com sucesso"
+        ];
+    }else{
+        $response = [
+            "error" => true,
+            "mensagem" => "Erro ao cadastrar produto"
+            ];
+    }
+
+    $response = [
+        "error" => false,
+        "mensagem" => "Produto cadastrado com sucesso"
+    ];
+}else {
+    $response = [
+        "error" => true,
+        "mensagem" => "Erro ao cadastrar produto"
+    ];
+}
+
+http_response_code(200);
+echo json_encode($response);
